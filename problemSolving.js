589843e83b1079b64d0a423e9cf609c8744bc331
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-08-10 13:29:22 +0600

    problem 8 to 18 has been solved

--- problemSolving.js
+++ problemSolving.js
@@ -89,8 +89,171 @@ console.log(fruits); */
 const myResult = gradingSystem(40);
 console.log(myResult); */
 
+/*-------------------------- PROBLEM NO 8 --------------------------------------*/
+
+/* function largestNumber(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        const largestNumber = num1;
+        return largestNumber;
+    }
+    else if (num2 > num1 && num2 > num3) {
+        const largestNumber = num2;
+        return largestNumber;
+    }
+    else {
+        const largestNumber = num3;
+        return largestNumber;
+    }
+}
+const largestValue = largestNumber(100, 200, 310);
+console.log(largestValue); */
 
 
+/*----------------------------- PROBLEM -9 --------------------------------*/
+
+/* function isIsosceles(side1, side2, side3) {
+    if (side1 === side2 && side1 === side3 && side2 === side3) {
+        return "This triange is not a isosceles";
+    }
+    else if (side1 === side2) {
+        return "This triange is isosceles";
+    }
+    else if (side1 === side3) {
+        return "This triange is isosceles";
+    }
+    else if (side2 == side3) {
+        return "This triange is isosceles";
+    }
+    else {
+        return "This triange is not an isosceles";
+    }
+}
+
+const isosceles = isIsosceles(30, 20, 30);
+console.log(isosceles); */
+
+/*---------------------------------- PROBLEM -10 -------------------------------*/
+
+/* function resultSystem(marks) {
+    if (marks < 0 || marks > 100) {
+        return "Out of mank range!";
+    }
+    else if (marks < 50) {
+        return "Your grade point is F";
+    }
+    else if (marks >= 50 && marks < 60) {
+        return "Your grade point is D";
+    }
+    else if (marks >= 60 && marks < 70) {
+        return "Your grade point is C";
+    }
+    else if (marks >= 70 && marks < 80) {
+        return "Your grade point is B";
+    }
+    else if (marks >= 80 && marks < 90) {
+        return "Your grade point is A";
+    }
+    else if (marks >= 90) {
+        return "Your grade point is A+";
+    }
+}
+
+const myResult = resultSystem(10);
+console.log(myResult); */
 
 
+/*-------------------------------- PROBLEM -11 --------------------------------*/
 
+// let signal = "yallow";
+
+// if (signal === "red") {
+//     console.log("You can't cross the road!");
+// }
+// else if (signal === "yellow") {
+//     console.log("You shoul be stop!");
+// }
+// else if (signal === "green") {
+//     console.log("You can move in the road!");
+// }
+// else {
+//     console.log("Your inuput is wrong");
+// }
+
+/*------------------------------------ PROBLEM -12 ------------------------------*/
+
+/* const tasks = ['unlock video on 8pm', 'watch video and practice task', 'takes notes when watching video', 'After end the module you will do practice task', 'If you can not understand anything you should join support session'];
+
+for (let i = 1; i <= 10; i++) {
+    console.log(tasks);
+} */
+
+
+/*-------------------------------- PROBLEM -13 ---------------------------------*/
+
+// const tasks = ['unlock video on 8pm', 'watch video and practice task', 'takes notes when watching video', 'After end the module you will do practice task', 'If you can not understand anything you should join support session'];
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(tasks);
+//     i++;
+// }
+
+/*------------------------------- PROBLEM -14 -------------------------------*/
+/* const tasks = ['unlock video on 8pm', 'watch video and practice task', 'takes notes when watching video', 'After end the module you will do practice task', 'If you can not understand anything you should join support session'];
+
+let i = 10;
+while (i >= 1) {
+    console.log(tasks);
+    i--;
+} */
+
+
+/*-------------------------- PROBLEM -15 -------------------------------*/
+
+/* const tasks = ['unlock video on 8pm', 'watch video and practice task', 'takes notes when watching video', 'After end the module you will do practice task', 'If you can not understand anything you should join support session'];
+
+for (let i = 10; i >= 1; i--) {
+    console.log(tasks);
+} */
+
+
+/*---------------------------------- PROBLEM -16 -----------------------------*/
+
+/* const money = 81000;
+
+if (money > 80000) {
+    console.log('I will buy mac');
+}
+else if (money > 60000) {
+    console.log('I will buy gaming laptop');
+}
+else if (money > 40000) {
+    console.log('I will buy lenevo youga');
+}
+else if (money > 20000) {
+    console.log('I will buy used laptop');
+}
+else {
+    console.log('I will do my work on mobile.');
+} */
+
+/*-------------------------------- PROBLEM -17 -----------------------------*/
+
+/* const mon = "Ajke amar mon valo ney";
+
+for (let i = 1; i < 40; i++) {
+    console.log(mon);
+} */
+
+
+/*-------------------------------- PROBLEM -18 -----------------------------*/
+
+/* const mon = "Ajke amar mon valo ney";
+let i = 1;
+while (i < 40) {
+    console.log(mon, i);
+    i++;
+} */
+
+
+/*-------------------------------- PROBLEM -19 ------------------------------*/
